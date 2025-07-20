252db2b62625c69fc199e3ae54e27eb9abb4238e
lexazare33 <lexazare2789@gmail.com>
2025-07-20 04:51:07 +0300

    поиск, категории, фотки, страницы товара
    Короче все криво немного, если хотите переделывайте, можете проверитьче как

---
+++ data.js
@@ -0,0 +1,53 @@
+// Файл с данными товаров
+const products = [
+  {
+    id: 1,
+    name: "Бобриная Хатка",
+    price: 1699,
+    oldPrice: 1999,
+    discount: "-15%",
+    image: "https://ilva.by/images/stories/berezinski_zapovednik/Muzeum/11.jpg", // Замените на реальный URL
+    description: "Уютная хатка для вашего бобра. Изготовлена из натуральных материалов. Размеры: 100x80x60 см.",
+    category: "Лисы"
+  },
+  {
+    id: 2,
+    name: "Домик для хорька",
+    price: 1899,
+    oldPrice: 2199,
+    discount: "-14%",
+    image: "https://live.staticflickr.com/2283/3533484190_419115ee56.jpg", // Замените на реальный URL
+    description: "Многоуровневый домик для хорька с гамаком и тоннелями. Размеры: 80x60x100 см.",
+    category: "Кошки"
+  },
+  {
+    id: 3,
+    name: "Гнездо для попугая",
+    price: 899,
+    oldPrice: 1199,
+    discount: "-25%",
+    image: "https://avatars.mds.yandex.net/i?id=94b23cb7be64e6b997dc4610032b5003_l-10752210-images-thumbs&n=13", // Замените на реальный URL
+    description: "Уютное гнездышко для попугаев средних пород. Диаметр: 40 см.",
+    category: "Птицы"
+  },
+  {
+    id: 4,
+    name: "Лежанка для кошки",
+    price: 1099,
+    oldPrice: 1499,
+    discount: "-27%",
+    image: "https://cdn1.ozone.ru/s3/multimedia-j/6181764199.jpg", // Замените на реальный URL
+    description: "Мягкая лежанка для кошки с бортиками. Размер: 50x40 см.",
+    category: "Кошки"
+  },
+  {
+    id: 5,
+    name: "Будка для собаки",
+    price: 3199,
+    oldPrice: 3999,
+    discount: "-20%",
+    image: "https://zveroteka.ru/wp-content/uploads/2021/03/Budki-16.jpg", // Замените на реальный URL
+    description: "Прочная будка для собак средних пород. Размеры: 120x80x100 см.",
+    category: "Собаки"
+  }
+];
